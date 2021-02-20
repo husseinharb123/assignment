eb914f4a131e650e6b47a8622eaac67fb5760d2d
husseinharb123 <79364658+husseinharb123@users.noreply.github.com>
2021-02-20 16:27:44 +0200

    Add files via upload

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket 
+from datetime import datetime
+
+request_string= "hi my name is hussein harb "  # messsage to be sent 
+serverip = str(input("Enter the IP address of the server : ") )   # server adddress                    
+serverPort =int(input("Enter the port number  of the server : ")   )  # server port number 
+
+clientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) # create UDP socket of client 
+
+RTT_total=0
+i =0
+for i in range(5): # sending message  5 times
+   
+    start = datetime.now()  # start couting 
+    clientSocket.sendto(request_string.encode(),(serverip, serverPort))
+    recmsg, serverAddress = clientSocket.recvfrom(4094)
+    end = datetime.now() # end counting 
+    
+    RTT = end - start  #roundtrip time
+    time = int(RTT.microseconds)  # time in microsecond
+    i+=1
+    print(f"RTT ({i}) : {time} μs ") # print RTT for each rely/rec pair
+    RTT_total+= RTT   #  total RTT 
+
+print(F"AVERGE RTT : {RTT_total / 5} μs ")  # print averge RTT
+
+clientSocket.close()  #close the socket 
---
+++ server.py
@@ -0,0 +1,22 @@
+import socket
+import time
+
+date =("time.ctime() : %s" % time.ctime())[14:]   # local time and date
+
+
+local_ip = "127.0.0.1"  # server address
+port = 7210     # port mumber
+receive_buffer_size = 4094 # size
+
+mysocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) # create  UDP socket for server
+
+mysocket.bind((local_ip, port)) # bind the socket 
+
+
+while True:
+    message, address_client = mysocket.recvfrom(receive_buffer_size)  #recive from client 
+   
+    print (f"Date : {date}") # print local time and date 
+    
+    if message:
+        mysocket.sendto(message, address_client)  #resend the recieved message 
